1ccd014bf23f73b6235b1f317252128f3ef6d80f
Ihor Malovanyi <ihor.malovanyi@fintech-farm.com>
2022-03-10 10:40:40 +0200

    [initial] project created

---
+++ CountiesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountiesApp
+//
+//  Created by Ihor Malovanyi on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
